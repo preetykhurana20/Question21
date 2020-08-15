5d4699e10fed6fd0c0317f987caed81f14c4b896
Preety <preety.bramton20@gmail.com>
2020-08-15 10:26:15 -0400

    first commit

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp21
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+		}
+	}
+	public class Gen<T>
+	{
+		private Array[] _B;
+		private int _addP = 0;
+		private int _getP = 0;
+
+		public Gen()
+		{
+			_B = new Array[100];
+		}
+
+		public void Add(Array item)
+		{
+			_B[_addP] = item;
+			_addP++;
+		}
+		public T Get()
+		{
+			T item = _B[_getP];
+			_getP++;
+			return item;
+		}
+
+
+	}
+}
